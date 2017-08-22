1f68db7f1c63042e8021907ac17c0a79be9121f1
Kerryn Donovan <kerryn.donovan@gmail.com>
2017-08-22 20:02:48 +0100

    reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822180601) do
+ActiveRecord::Schema.define(version: 20170822190022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20170822180601) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "run_id"
+    t.index ["run_id"], name: "index_run_owner_reviews_on_run_id"
     t.index ["user_id"], name: "index_run_owner_reviews_on_user_id"
   end
 
@@ -71,6 +73,7 @@ ActiveRecord::Schema.define(version: 20170822180601) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "run_owner_reviews", "runs"
   add_foreign_key "run_owner_reviews", "users"
   add_foreign_key "runs", "users"
 end
