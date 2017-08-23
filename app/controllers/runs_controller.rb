a8b4fd3f11e46c2b321a054ef559b87ee399b5e6
mohamed almoaakad <mohamedmoaakad93mm@gmail.com>
2017-08-23 17:34:13 +0100

    create map

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,5 +1,12 @@
 class RunsController < ApplicationController
   def index
+    @runs = Run.all
 
+    @runs = Run.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@runs) do |run, marker|
+      marker.lat run.latitude
+      marker.lng run.longitude
+    end
   end
 end
