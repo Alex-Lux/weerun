e0e7372cef3f4bdd4dbc4b56383c3ef0b8ec6945
Alexandra Vidic <alexandravidic@yahoo.fr>
2017-08-23 14:37:30 +0100

    Image upload and Cloudinary key for Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true, length: { minimum: 2 }
+  #validates :first_name, presence: true
+  #validates :last_name, presence: true, length: { minimum: 2 }
   validates :email, uniqueness: true, presence: true, format: { with: /\A.*@.*\.com\z/ }
-  validates :phone_number, presence: true
-  validates :language_spoken, presence: true
-  validates :birthdate, presence: true
-  validates :description, presence: true
-  validates :gender, presence: true
-  validates :pace, presence: true
-  validates :photo, presence: true
+  #validates :phone_number, presence: true
+  #validates :language_spoken, presence: true
+  #validates :birthdate, presence: true
+  #validates :description, presence: true
+  #validates :gender, presence: true
+  #validates :pace, presence: true
+  #validates :photo, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
