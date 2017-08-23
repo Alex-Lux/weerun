e163e30bef314773bea335711ccf0a3ddceb8e79
Fernando Morgado <github@fernandomorgado.eu>
2017-08-23 12:50:00 +0100

    created the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :runs do
+    resources :run_bookings, only: [ :new, :create ]
+    resources :runner_reviews, only: [ :new, :create ]
+    resources :run_owner_reviews, only: [ :new, :create ]
+
+  end
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  patch '/run_bookings/:id/accept', to: 'run_bookings#accept'
+  patch '/run_bookings/:id/decline',  to: 'run_bookings#decline'
+
+
 end
