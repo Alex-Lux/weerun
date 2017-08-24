424cc7cabce75c0e179bdfa7aecdb73e354207c6
Alexandra Vidic <alexandravidic@yahoo.fr>
2017-08-24 11:14:05 +0100

    Grammar correction in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ puts 'Creating 10 fake runners...'
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     language_spoken: ["english", "french", "italian", "arabic", "german", "portuguese", "dutch", "japanese"].sample,
-    gender: ["men", "woman"].sample
+    gender: ["male", "female"].sample
   )
   user.save!
 
